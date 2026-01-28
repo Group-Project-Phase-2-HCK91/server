0f23cac8f588a9fdfcdeb2949a6637b2956894dd
Dheovani Aditya Fasha Muhammad <dheovaniadityafashamuhammad@Dheovanis-MacBook-Air.local>
2026-01-28 18:01:24 +0700

    menambahkan socket ke app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const io = new Server(server, {
     origin: "*"
   }
 });
-// const port = 3000;
+// const port = 3000; 
 const multer = require('multer');
 const cors = require('cors'); 
 
@@ -22,7 +22,7 @@ const upload = multer({
     limits: { fileSize: 5 * 1024 * 1024 } 
 });
 
-// Controller
+// CONTROLLER
 const UserController = require('./controllers/userController');
 const MessageController = require('./controllers/messageController');
 
